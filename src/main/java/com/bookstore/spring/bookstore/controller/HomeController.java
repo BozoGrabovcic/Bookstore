4a5ddfb6bceb19da41e22b13b952dbeee77147f0
BozidarGrabovcic <grabovcic93@gmail.com>
2019-11-07 11:05:46 +0100

    mapping for listOfCreditCards, and listOfShippingAddresses, and addNewCreditCard

--- src/main/java/com/bookstore/spring/bookstore/controller/HomeController.java
+++ src/main/java/com/bookstore/spring/bookstore/controller/HomeController.java
@@ -30,6 +30,8 @@ import com.bookstore.spring.bookstore.domain.Book;
 import com.bookstore.spring.bookstore.domain.PasswordResetToken;
 import com.bookstore.spring.bookstore.domain.Role;
 import com.bookstore.spring.bookstore.domain.User;
+import com.bookstore.spring.bookstore.domain.UserBilling;
+import com.bookstore.spring.bookstore.domain.UserPayment;
 import com.bookstore.spring.bookstore.domain.UserRole;
 import com.bookstore.spring.bookstore.domain.UserShipping;
 import com.bookstore.spring.bookstore.service.impl.BookService;
@@ -95,6 +97,89 @@ public class HomeController {
 		 return "myProfile";
 		 
 	}
+	
+	@RequestMapping("/listOfCreditCards")
+	public String listOfCreditCards(
+			Model model, Principal principal, HttpServletRequest request) {
+		User user = userService.findByUsername(principal.getName());
+		model.addAttribute("user",user);
+		model.addAttribute("userPaymentList", user.getUserPaymentList());
+		model.addAttribute("userShippingList", user.getUserShippingList());
+		//model.addAttribute("orderList", user.orderList());
+		
+		model.addAttribute("listOfCreditCards", true);
+		model.addAttribute("classActiveBilling", true);
+		model.addAttribute("listOfShippingAddresses",true);
+		
+		return "myProfile";
+	}
+	
+	@RequestMapping("/listOfShippingAddresses")
+	public String listOfShippingAddresses(
+			Model model, Principal principal, HttpServletRequest request) {
+		User user = userService.findByUsername(principal.getName());
+		model.addAttribute("user",user);
+		model.addAttribute("userPaymentList", user.getUserPaymentList());
+		model.addAttribute("userShippingList", user.getUserShippingList());
+		//model.addAttribute("orderList", user.orderList());
+		
+		model.addAttribute("listOfCreditCards", true);
+		model.addAttribute("classActiveBilling", true);
+		model.addAttribute("listOfShippingAddresses",true);
+		
+		return "myProfile";
+	}
+	
+	@RequestMapping("addNewCreditCard")
+	public String addNewCreditCard(
+			Model model, Principal principal) {
+		User user = userService.findByUsername(principal.getName());
+		model.addAttribute("user", user);
+		
+		model.addAttribute("addNewCreditCard", true);
+		model.addAttribute("classActiveBilling", true);
+		model.addAttribute("listOfShippingAddresses",true);
+		
+		UserBilling userBilling = new UserBilling();
+		UserPayment userPayment = new UserPayment();
+		
+		model.addAttribute("userBilling", userBilling);
+		model.addAttribute("userPayment", userPayment);
+		
+		List<String> stateList = USConstants.listOfUSStatesCode;
+		Collections.sort(stateList);
+		model.addAttribute("stateList", stateList);
+		model.addAttribute("userPaymentList", user.getUserPaymentList());
+		model.addAttribute("userShippingList", user.getUserShippingList());
+		//model.addAttribute("orderList", user.orderList());
+		
+		return "myProfile";
+	}
+	
+	@RequestMapping("addNewShippingAddress")
+	public String addNewShippingAddress(
+			Model model, Principal principal) {
+		User user = userService.findByUsername(principal.getName());
+		model.addAttribute("user", user);
+		
+		model.addAttribute("addNewShippingAddress", true);
+		model.addAttribute("classActiveShipping", true);
+		
+		UserShipping userShipping = new UserShipping();
+		
+		model.addAttribute("userShipping", userShipping);
+		
+		List<String> stateList = USConstants.listOfUSStatesCode;
+		Collections.sort(stateList);
+		model.addAttribute("stateList", stateList);
+		model.addAttribute("userPaymentList", user.getUserPaymentList());
+		model.addAttribute("userShippingList", user.getUserShippingList());
+		//model.addAttribute("orderList", user.orderList());
+		
+		return "myProfile";
+	}
+	
+	
 	
 	@RequestMapping("/bookshelf")
 	public String bookshelf(Model model) {
